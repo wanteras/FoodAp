b9e2f010ec336ae8f46d80ee46b2dfe6751567b9
Tolea <stavila.tolea@mail.ru>
2022-05-13 12:43:37 +0300

    OrderDeliveryScreen and HomeScreen are made

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+
+    View,
+    Text,
+    
+} from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from "@react-navigation/native"
+
+const OrderDelivery = () => {
+
+    return(
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    );
+}
+
+export default OrderDelivery;
